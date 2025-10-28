0b9af6f97f8a86110645bc9e4497e5f0872b3396
Chain Platform <support@chainplatform.net>
2025-10-28 17:56:07 +0700

    change logic toggle

--- src/Accordion.js
+++ src/Accordion.js
@@ -1,48 +1,28 @@
 import { Component } from 'react';
-import {
-    Animated,
-    View,
-    Pressable,
-    Text,
-    Easing,
-    Platform
-} from 'react-native';
+import { Animated, View, Pressable, Text, Platform } from 'react-native';
 
 export default class Accordion extends Component {
     constructor(props) {
         super(props);
+
         this.state = {
             open: props.initiallyOpen || false,
-            contentHeight: 0,
         };
-        this.animatedHeight = new Animated.Value(props.initiallyOpen ? 1 : 0);
+
         this.rotateAnim = new Animated.Value(props.initiallyOpen ? 1 : 0);
     }
 
     toggle = () => {
         const { duration = 250 } = this.props;
         const { open } = this.state;
+        const toValue = open ? 0 : 1;
         this.setState({ open: !open }, () => {
-            Animated.timing(this.animatedHeight, {
-                toValue: this.state.open ? 1 : 0,
-                duration,
-                easing: Easing.out(Easing.ease),
-                useNativeDriver: false,
-            }).start();
             Animated.timing(this.rotateAnim, {
-                toValue: this.state.open ? 1 : 0,
+                toValue,
                 duration,
-                easing: Easing.out(Easing.ease),
-                useNativeDriver: Platform.OS !== 'web',
+                useNativeDriver: Platform.OS !== "web"
             }).start();
         });
-    };
-
-    handleContentLayout = (e) => {
-        const { height } = e.nativeEvent.layout;
-        if (height !== this.state.contentHeight) {
-            this.setState({ contentHeight: height });
-        }
     };
 
     render() {
@@ -53,28 +33,20 @@ export default class Accordion extends Component {
             style,
             titleStyle,
             contentStyle,
-            butotnStyle
+            buttonStyle,
         } = this.props;
-        const { contentHeight } = this.state;
+        const { open } = this.state;
         const rotate = this.rotateAnim.interpolate({
             inputRange: [0, 1],
-            outputRange: ['0deg', '90deg'],
+            outputRange: ['0deg', '90deg']
         });
-        const heightStyle = contentHeight > 0
-            ? {
-                height: this.animatedHeight.interpolate({
-                    inputRange: [0, 1],
-                    outputRange: [0, contentHeight],
-                })
-            } : {};
-
         return (
             <View style={[{
                 borderRadius: 12,
-                overflow: 'hidden',
                 borderColor: '#ddd',
                 borderWidth: 1,
-                marginBottom: 8
+                marginBottom: 8,
+                overflow: 'hidden',
             }, style]}>
                 <Pressable onPress={this.toggle} style={[{
                     flexDirection: 'row',
@@ -82,28 +54,20 @@ export default class Accordion extends Component {
                     alignItems: 'center',
                     paddingVertical: 14,
                     paddingHorizontal: 16,
-                    backgroundColor: '#fafafa'
-                }, butotnStyle]}>
+                    backgroundColor: '#fafafa',
+                }, buttonStyle]}>
                     <Text style={[{ fontSize: 16, fontWeight: '600', color: '#333' }, titleStyle]}>
                         {title}
                     </Text>
                     <Animated.View style={{ transform: [{ rotate }] }}>
-                        <Text style={[{ fontSize: 18, color: '#00C853' }, iconStyle]}>
-                            {'›'}
-                        </Text>
+                        <Text style={[{ fontSize: 18, color: '#00C853' }, iconStyle]}>›</Text>
                     </Animated.View>
                 </Pressable>
-                <Animated.View style={[{ overflow: 'hidden', opacity: this.animatedHeight }, heightStyle]}>
-                    <View
-                        onLayout={this.handleContentLayout}
-                        style={[{
-                            paddingHorizontal: 16,
-                            paddingVertical: 10,
-                            backgroundColor: '#fff',
-                        }, contentStyle]}>
-                        {children}
-                    </View>
-                </Animated.View>
+                {open && (<View style={[{
+                    paddingHorizontal: 16,
+                    paddingVertical: 10,
+                    backgroundColor: '#fff',
+                }, contentStyle]}>{children}</View>)}
             </View>
         );
     }
